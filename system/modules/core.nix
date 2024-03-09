ecc2782de6b5e842fdb7ca66058c9e6b2a753ac0
stormytuna <stormytuna@outlook.com>
2024-03-09 21:37:16 +0000

    Actually finalise Nix stuffs

--- system/modules/core.nix
+++ system/modules/core.nix
@@ -7,7 +7,9 @@
     grc # fish shell colourisation, even though we don't have fish here we can't install it using 
     tldr
     git
+    gh
     #xfce.thunar # TODO: Alternative? since i wanna use hyprland not xfce?
     firefox
+    bitwarden
   ];
 }
